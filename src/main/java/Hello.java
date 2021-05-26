0c03281abc11dff5bf954eb4e64da0a8dc020a64
tulayozbay <82976910+tulayozbay@users.noreply.github.com>
2021-05-26 01:19:20 -0400

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,10 @@ public class Hello {
 
         System.out.println("Developing hello feature");
 
+        System.out.println("Local Repo is already published to github");
+
+        System.out.println("Doing more work");
+
     }
 
 }
